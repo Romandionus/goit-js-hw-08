561ae3303b9ed7b5b5b0ae407b77342c8a7b3907
Roman Stankevych <romandionus@gmail.com>
2022-02-20 23:57:23 +0200

    video player editing js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,36 @@
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
-
-const onPlay = function(data) {
-    console.log('played the video!');
-         // data is an object containing properties specific to that event
-   };
-player.on('play', onPlay);
-// const currentTime = function () {
-//     console.dir(currentTime.seconds)
-// }
-// currentTime.addEventListenr(`currentTime`, currentTime)
+const player = new Vimeo.Player(iframe);
 
 player.on('timeupdate', function (data) {
     console.log(data.seconds)
     JSON.stringify("videoplayer-current-time", data.seconds)
-    // data is an object containing properties specific to that event
     localStorage.setItem("videoplayer-current-time", data.seconds);
 })
 
+
+const currentTime = localStorage.getItem("videoplayer-current-time")
+const parsedCurrentTime = JSON.parse(currentTime)
+console.log(parsedCurrentTime)
+
+player.setCurrentTime(parsedCurrentTime).then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+// currentTime.addEventListener(  'input',  throttle(e => {}, 10000))
+
+
+
+// const onPlay = function(data) {
+//     console.log('played the video!');
+// };
+// player.on('play', onPlay);
