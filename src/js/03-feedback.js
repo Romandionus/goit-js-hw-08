f847560561cf053b06b08027b0e33f64e0de2f8a
Roman Stankevych <romandionus@gmail.com>
2022-02-21 22:32:34 +0200

    start task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(`form`);
+const input = document.querySelector(`input`);
+// const textarea = document.querySelector(`textarea`);
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const SECOND_KEY = "message-value";
 
+updateInput();
+// form.addEventListener(`submit`, saveMessage);
+form.addEventListener(`input`, saveMessage);
+
+function saveMessage(evt) {
+    evt.preventDefault();
+    localStorage.setItem(SECOND_KEY, form.elements.message.value);
+    localStorage.setItem(LOCALSTORAGE_KEY, form.elements.email.value);
+    updateInput();
+    form.reset
+}
+
+function updateInput() {
+    input.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
+    // textarea.textContent = localStorage.getItem(LOCALSTORAGE_KEY)|| "";
+}
