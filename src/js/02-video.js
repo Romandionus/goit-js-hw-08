45c79c17c884f37b6b226e753859e374befd0510
Roman Stankevych <romandionus@gmail.com>
2022-02-21 19:59:55 +0200

    task 2
    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,35 +2,22 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', function (data) {
+player.on('timeupdate', throttle((data) => {
     console.log(data.seconds)
     JSON.stringify("videoplayer-current-time", data.seconds)
     localStorage.setItem("videoplayer-current-time", data.seconds);
-})
-
+    }, 1000))
 
 const currentTime = localStorage.getItem("videoplayer-current-time")
 const parsedCurrentTime = JSON.parse(currentTime)
 console.log(parsedCurrentTime)
 
 player.setCurrentTime(parsedCurrentTime).then(function (seconds) {
-    // seconds = the actual time that the player seeked to
 }).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
             break;
         default:
-            // some other error occurred
             break;
     }
 });
-
-// currentTime.addEventListener(  'input',  throttle(e => {}, 10000))
-
-
-
-// const onPlay = function(data) {
-//     console.log('played the video!');
-// };
-// player.on('play', onPlay);
